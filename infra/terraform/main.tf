1d63eb949e1aae3adcd58bcf5610d1fe0feea2b4
amit180914 <233851261+amit180914@users.noreply.github.com>
2026-06-10 23:08:53 +0530

    Add Terraform configuration for GCP infrastructure setup

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -82,6 +82,12 @@ resource "google_service_account_iam_member" "github_impersonation" {
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
 }
 
+resource "google_service_account_iam_member" "github_token_creator" {
+  service_account_id = google_service_account.github_deployer.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
+}
+
 resource "google_service_account_iam_member" "gke_workload_identity" {
   service_account_id = google_service_account.gke_app.name
   role               = "roles/iam.workloadIdentityUser"
